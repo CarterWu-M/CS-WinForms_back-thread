8d337e7a26fa65707471da45eb2b335a91febd20
CarterWu-M <hellocarterwu@gmail.com>
2024-10-27 14:19:54 +0800

    Add files via upload

---
+++ background task/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace background_thread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(274, 167);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(151, 53);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(70, 167);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(151, 53);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("PMingLiU", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt1.Location = new System.Drawing.Point(139, 81);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(197, 25);
+            this.txt1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 276);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txt1;
+    }
+}
+
---
+++ background task/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace background_thread
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource _cancellationTokenSource;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.BackColor = Color.LightGray;
+            btnCancel.BackColor = Color.LightBlue;
+
+            await Task.Run(() => {
+                testTask();
+            });
+
+            btnStart.BackColor = Color.LightGreen;
+            btnCancel.BackColor = Color.LightGray;
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this._cancellationTokenSource?.Cancel();
+        }
+        private void testTask()
+        {
+            this._cancellationTokenSource?.Dispose();
+            this._cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                for (uint i = 0; 10 > i; i++)
+                {
+                    Thread.Sleep(1000);
+                    txt1.Invoke(new Action(() => txt1.Text = i.ToString()));
+                    txt1.Invoke(new Action(() => txt1.Refresh()));
+                    this._cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("Task has been Cancend!");
+            }
+        }
+    }
+}
